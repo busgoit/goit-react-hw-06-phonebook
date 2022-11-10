9b70cf26b9b8f8c4714250f9ef116c34f7d7e275
Vasil Karpliuk <bus.go.it@gmail.com>
2022-11-10 18:41:30 +0200

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const initialContacts = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -29,12 +40,38 @@ const filterSlice = createSlice({
   },
 });
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const { setFilter } = filterSlice.actions;
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE],
+      },
+    });
+  },
 });
+
+export const persistor = persistStore(store);
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsSlice.reducer,
+//     filter: filterSlice.reducer,
+//   },
+// });
